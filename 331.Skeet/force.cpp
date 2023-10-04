2234387241a5b3a3d61eb4041c4dd13e431ef397
Efrain Gomez Fajardo <gom21023@byui.edu>
2023-10-04 09:40:50 -0600

    Added force.cpp

---
+++ 331.Skeet/force.cpp
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Source File:
+ *    Force : Natural effects that affect Bird
+ * Author:
+ *    Efrain Gomez Fajardo
+ * Summary:
+ *    Changes that apply to Bird by just existing
+ ************************************************************************/
+#include "force.h"
+
+
+void StandardDrag::apply(Bird* b)
+{
+}
